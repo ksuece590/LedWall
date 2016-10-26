216b09fe6b215a578d3c5c88fc3e3063fc8368d2
ksuece590 <ksu.ece590.ledwall@gmail.com>
2016-10-26 16:19:17 -0500

    attempting to use pointers

--- Arduino/teensy_rev2/teensy_rev2.ino
+++ Arduino/teensy_rev2/teensy_rev2.ino
@@ -14,7 +14,8 @@ const int bytesPerColor = 3;
 const int headerSize = 4;
 const int maxRows = 8;
 const int numLEDs = ledsPerStrip * maxRows;
-const int maxBufSize = (bytesPerColor * ledsPerStrip * maxRows) + headerSize;
+const int maxPayloadSize = (bytesPerColor * numLEDs); 
+const int maxBufSize = maxPayloadSize + headerSize;
 const int baudRate = 921600;
 const unsigned long resetTime = 10000;
 
@@ -23,16 +24,40 @@ int drawingMemory[ledsPerStrip*6];
 const int config = WS2811_GRB | WS2811_800kHz;
 OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
 
+byte ACK = 0x13;
+
+byte* pkt_buf[2];
+
 byte inputBuffer[maxBufSize];
+
+/*
+
+0 - 0xFFFF
+1 - state
+2 - payload size
+3 - payload size
+4 - payload
+...
+N - payload 
+
+*/
+
 byte outputBuffer[2];
 int serialBufIdx;
 int serialBufLen;
-int bufferPtr;
+int * bufPtr;
 bool serialPktEnd;
 unsigned long startTime;
 bool serialReset;
 
+
+void setOff();
+void setWall(byte grid[]);
+void setWallPtr(int * bufPtr);
+void showWall();
 void bootScreen();
+void resetScreen();
+void serialInterupt();
 
 void setup()
 {
@@ -58,11 +83,11 @@ void loop()
 		serialBufIdx = 0;
 
 		setWall(inputBuffer);
+		//setWallPtr(inputBuffer);
 	}
 
 	serialInterupt();
 }
-
 
 void serialEvent()
 {
@@ -84,17 +109,6 @@ void serialEvent()
 	}
 }
 
-void serialInterupt()
-{
-	if(abs(millis() - startTime) >= resetTime)
-	{
-		Serial.end();
-		resetScreen();
-		memset(inputBuffer, 0, maxBufSize);
-		serialBufIdx = 0;
-	}
-}
-
 void setOff()
 {
 	for(int i = 0; i < numLEDs; i++)
@@ -105,21 +119,47 @@ void setOff()
 	leds.show();
 }
 
+void setWallPtr(int * bufPtr)
+{
+	int * ptr = bufPtr;
+
+	int r, g, b;
+
+	for int(i = 0; i < numLEDs; i++)
+	{
+		if(maxPaylodSize > (ptr - bufPtr)) { return; }
+
+		r = *ptr;
+		g = *(ptr+1);
+		b = *(ptr+2);
+
+		leds
+
+		ptr += 3;
+		leds.setPixel(ledIdx, r | g | b);		
+	}
+
+	showWall();
+}
+
 void setWall(byte grid[])
 {
+	int r, g, b;
 	int ledIdx = 0;
 
 	for(int i = 4; i < maxBufSize; ledIdx++)
 	{
-		int r = grid[i++] << 16;
-		int g = grid[i++] << 8;
-		int b = grid[i++];
+		r = grid[i++] << 16;
+		g = grid[i++] << 8;
+		b = grid[i++];
 
 		leds.setPixel(ledIdx, r | g | b);
 	}
 
-	leds.show();
+	showWall();
 }
+
+void showWall() { leds.show(); }
 
 void bootScreen()
 {
@@ -148,7 +188,6 @@ void bootScreen()
 	delay(1000);
 }
 
-
 void resetScreen()
 {
 	for(int i = 0; i < numLEDs; i++)
@@ -157,4 +196,15 @@ void resetScreen()
 	}
 
 	leds.show();
+}
+
+void serialInterupt()
+{
+	if(abs(millis() - startTime) >= resetTime)
+	{
+		Serial.end();
+		resetScreen();
+		memset(inputBuffer, 0, maxBufSize);
+		serialBufIdx = 0;
+	}
 }
