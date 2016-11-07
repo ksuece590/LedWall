bd42b789c9435b65afa7f7199185f677f7bd7d94
ksuece590 <ksu.ece590.ledwall@gmail.com>
2016-11-07 13:15:45 -0600

    Allow hotplugging Teensys

--- LedWallProtocol/LedWallProtocol/LedWallTeensyDriver.cs
+++ LedWallProtocol/LedWallProtocol/LedWallTeensyDriver.cs
@@ -10,51 +10,110 @@ namespace LedWallProtocol
 {
     class LedWallTeensyDriver
     {
+        /// <summary>
+        /// The number of strips controlled by one Teensy
+        /// </summary>
         public const int StripsPerTeensy = 8;
 
-        SerialPort _serialPort;
+        /// <summary>
+        /// Serial communication's baud rate
+        /// </summary>
+        public int Baud { get; private set; }
 
-        byte[] ackbuf;
+        /// <summary>
+        /// Serial port's name
+        /// </summary>
+        public string Port { get; private set; }
 
+        /// <summary>
+        /// The actual serial port communicating with the Teensy
+        /// </summary>
+        private SerialPort _serialPort;
+
+        /// <summary>
+        /// Construct new LedWallTeensyDriver
+        /// </summary>
+        /// <param name="portName">Serial port's name</param>
+        /// <param name="baud">Serial communication's baud rate</param>
         public LedWallTeensyDriver(string portName, int baud)
         {
-            ackbuf = new byte[10];
+            // Save port info in case of close
+            this.Baud = baud;
+            this.Port = portName;
 
-            try
-            {
-                _serialPort = new SerialPort();
-                _serialPort.PortName = portName;
-                _serialPort.BaudRate = baud;
-                _serialPort.WriteTimeout = SerialPort.InfiniteTimeout;
-                _serialPort.Open();
-            }
-            catch
-            {
-                _serialPort.Dispose();
-                _serialPort = null;
-            }
+            // Attempt to open
+            openPort(Port, Baud, out _serialPort);
         }
 
+        /// <summary>
+        /// Destructor
+        /// </summary>
         ~LedWallTeensyDriver()
         {
             if(_serialPort != null)
                 _serialPort.Close();
         }
 
-        public void Send(LedMessage message, Semaphore notify=null)
+        /// <summary>
+        /// Sends the given LedMessage to this Teensy
+        /// </summary>
+        /// <param name="message">The message to send</param>
+        /// <param name="notify">A semaphore to release upon write completion</param>
+        public void Send(LedMessage message, Semaphore notify = null)
         {
-            if (_serialPort == null) return;
+            // Only actually send data when serial port is available
+            if ((this._serialPort != null) || openPort(Port, Baud, out _serialPort))
+            {
+                byte[] buf = message.Serialize();
+                try
+                {
+                    _serialPort.Write(buf, 0, buf.Length);
+                }
+                catch
+                {
+                    _serialPort = null;
+                }
+            }
 
-            byte[] buf = message.Serialize();
-            _serialPort.Write(buf, 0, buf.Length);
+            // Release semaphore
             if (notify != null)
                 notify.Release();
         }
 
-        public byte[] WaitForAck()
+        /// <summary>
+        /// Opens a serial port with the given name at the given baud rate
+        /// </summary>
+        /// <param name="portName">Serial port's name</param>
+        /// <param name="baud">Serial communication's baud rate</param>
+        /// <param name="port">Output SerialPort</param>
+        /// <returns>Success/Failure</returns>
+        private bool openPort(string portName, int baud, out SerialPort port)
         {
-            _serialPort.Read(ackbuf, 0, 1);
-            return ackbuf;
+            // Local variables
+            SerialPort pt = null;
+            bool success = false;
+
+            // Init port
+            pt = new SerialPort();
+            pt.PortName = portName;
+            pt.BaudRate = baud;
+            pt.WriteTimeout = SerialPort.InfiniteTimeout;
+
+            // Attempt to open
+            try
+            {
+                pt.Open();
+                success = true;
+            }
+            catch
+            {
+                pt.Dispose();
+                pt = null;
+            }
+
+            // Assign output and return success/failure
+            port = pt;
+            return success;
         }
     }
 }
